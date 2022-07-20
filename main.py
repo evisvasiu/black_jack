fd5c701b65017c4aee1d9e9b56199f72cf111e82
evisvasiu <45014223+evisvasiu@users.noreply.github.com>
2022-07-20 17:22:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,19 @@ def sum_hand(hand):
 def choose_a(hand):
   s = sum_hand(hand)
   if s > 10:
-    if s == 22:
+    if (hand[0] == "A" or hand[1] == "A") and (hand[0] != hand[1]) and len(hand)<3:
+      cards["A"] = 11
+    elif (hand[0] == hand[1] == "A") and len(hand)<3:  #when first cards are "A"
       cards["A"] = 6
-    else:
+    elif (hand[0] == hand[1] == "A"):  #when three "A"
+      if hand[-1] == "A":
+        cards[hand[0]] = 11
+        cards["A"] = 1
+      elif cards[hand[-1]] == 10:
+        cards["A"] = 1
+      elif cards[hand[-1]] < 10:
+        cards["A"] = 6
+    if s > 21:
       cards["A"] = 1
   else:
     cards["A"] = 11
@@ -43,20 +53,25 @@ while start == "y":
   cards["A"] = 11
   for i in range(2):
     computer_cards.append(random.choice(list(cards)))
+  choose_a(computer_cards)  #check for double A
   sum_pc = sum_hand(computer_cards)
 
   #this loop will add cards to computer hand until sum of hands reach value 17
-  while sum_pc < 17:
+  while sum_pc < 17: 
     computer_cards.append(random.choice(list(cards)))
-    choose_a(computer_cards)  #this choose the next value of "A" based on the current sum
+    choose_a(computer_cards) 
     sum_pc = sum_hand(computer_cards)
   
   #player, first 2 cards
-  choose_a(player_cards) #reset "A" to 11  
+  cards["A"] = 11 #reset "A" to 11  
   for i in range(2):
     player_cards.append(random.choice(list(cards)))
+
+  choose_a(player_cards)  #check for double A
   sum_player = sum_hand(player_cards)
   
+  
+   
   print(f"You have: {player_cards}, score: {sum_player}")
   print(f"Dealer's first card is: {computer_cards[0]}\n")
   
@@ -71,7 +86,7 @@ while start == "y":
       print("It's a draw!")
     else:
       print("You win!")
-      print(f"Dealer has: {computer_cards}")
+      print(f"Dealer has: {computer_cards}, {sum_pc}")
   else:
     another_card = input("Type 'y' to get another card, or 'n' to pass: ")
   
@@ -79,19 +94,23 @@ while start == "y":
   while another_card == "y":
     clear()
     print(logo)
-    choose_a(player_cards)
     player_cards.append(random.choice(list(cards)))
+    choose_a(player_cards)
     sum_player = sum_hand(player_cards)
     
     if sum_player > 21:
       print(f"You have: {player_cards}, score: {sum_player}")
-      print(f"Dealer has: {computer_cards[:2]}")
+      if sum_hand(computer_cards[:2]) == 22:
+        cards["A"] = 6
+      else:
+        cards["A"] = 11
+      print(f"Dealer has: {computer_cards[:2]}, score: {sum_hand(computer_cards[:2])}")
       print("Bust! Dealer wins!")
       break
       
     elif sum_player == 21:
       print(f"You have: {player_cards}, score: {sum_player}")
-      print(f"Dealer has: {computer_cards}")
+      print(f"Dealer has: {computer_cards}, score: {sum_pc}")
       if sum_pc == 21:
         print("Push! It's a draw.")
       else:
